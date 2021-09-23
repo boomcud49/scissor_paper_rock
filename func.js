91b230342ba7cbf39c95232449f6a110e3695233
Boom <boomcud49@gmail.com>
2021-09-23 15:25:20 +0700

    go rock

---
+++ func.js
@@ -0,0 +1,64 @@
+function computerPlay(){
+    action = ["scissor","paper","rock"];
+    num = Math.floor(Math.random() * 3);
+    return action.slice(num,num+1);
+}
+console.log("hello");
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "draw"
+    }
+    else if (playerSelection == "scissor"){
+        if (computerSelection == "paper"){
+            return "win"
+        }
+        else{
+            return "lose"
+        }
+    }
+    else if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
+            return "win"
+        }
+        else{
+            return "lose"
+        }
+    }
+    else if (playerSelection == "rock"){
+        if (computerSelection == "scissor"){
+            return "win"
+        }
+        else {
+            return "lose"
+        }
+    }
+}
+console.log(playRound("rock","paper"));
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i=0;i<5;i++){
+        let playerSelection = prompt("What's your sign");
+        let computerSelection = computerPlay();
+        let result = playRound(playerSelection,computerSelection);
+        if( result == "win"){
+            playerScore += 1;
+        }
+        else if (result == "lose"){
+            computerScore += 1;
+        }
+        alert("You select! " + playerSelection);
+        alert("Computer select! " + computerSelection);
+        alert("You " + result);
+    }
+    if (playerScore > computerScore){
+        return "Player win"
+    }
+    else if (playerScore == computerScore){
+        return "draw"
+    }
+    else{
+        return "Player lose"
+    }
+}
+console.log(game());
